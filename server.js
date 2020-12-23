526ae86cb76aefd7e483f433d42b6f53f29f161a
eramos3 <cisco@Eders-MacBook-Pro.local>
2020-12-23 12:48:40 -0800

    updated separateData

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-const fs = require('fs');
-const path = require('path');
 const express = require('express');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 const apiRoutes = require('./routes/apiRoutes');
@@ -10,14 +9,11 @@ const htmlRoutes = require('./routes/htmlRoutes');
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
-const { animals } = require('./data/animals');
 app.use(express.static('public'));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
+// use apiroutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
